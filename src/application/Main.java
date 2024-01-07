7eadcd4e20145bdbbb29cc6eeffc23db1fad0cd1
Gustavo Biava <gustavo.rbiava@hotmail.com>
2024-01-06 22:42:46 -0300

    First class: Position

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,12 @@
 package application;
 
+import boardGame.Position;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Position position = new Position(8,1);
+        System.out.println(position);
+
     }
 }
