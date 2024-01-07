40ce2de2c76cb370540a0186b2661a47a2c6682c
Gustavo Biava <gustavo.rbiava@hotmail.com>
2024-01-07 18:06:52 -0300

    ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,9 +25,13 @@ public class ChessMatch {
          return mat;
     }
 
+    private void placeNewPiece(char column, int row, ChessPiece piece){
+        board.placePiece(piece, new ChessPosition(column,row).toPosition());
+    }
+
     public void initialSetup(){
-        board.placePiece(new Rook(board, Color.BLACK),new Position(2,4));
-        board.placePiece(new King(board, Color.BLACK),new Position(1,4));
+        placeNewPiece('a',8, new Rook(board, Color.BLACK));
+        placeNewPiece('a',1, new King(board, Color.BLACK));
     }
 
 
